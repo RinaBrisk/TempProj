c5254b9c102e8050e28c342008559567c9cde2a1
Rina <rina_sergeeva@bk.ru>
2020-04-01 15:10:22 +0300

    commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+        System.out.println(sum(2, 5));
+    }
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+}
