7c9d80b7278946ad9e2a61cba272973271f546a1
Rina <rina_sergeeva@bk.ru>
2020-04-27 16:58:11 +0300

    change

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -11,17 +11,12 @@ public class HelloWorld {
     private String report;
     private static Map<String, String> coverage;
 
-    private static final String TEST_COVERAGE = "TestCoverage";
-    private static final String JENKINS_URL = "http://localhost:8081/jenkins";
-    private static final String COVERAGE_REPORT_PATH = "C:/Users/Rina/.jenkins/workspace/TestCoverage/target/site/cobertura/frame-sourcefiles.html";
-    private static final String COVERAGE_REPORT_URL = JENKINS_URL + "/job/" + TEST_COVERAGE + "/lastSuccessfulBuild/artifact/target/site/cobertura/index.html";
-
-    public static void main(String[] argv) throws IOException {
+    public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println(sum(2, 5));
     }
 
-    public static int sum(int a, int b) throws IOException {
+    public static int sum(int a, int b) {
         return a + b;
     }
 
--- src/test/java/TestsInternal.java
+++ src/test/java/TestsInternal.java
@@ -1,12 +1,10 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
-
 public class TestsInternal {
 
     @Test
-    public void checkSum() throws IOException {
+    public void checkSum(){
         int result = HelloWorld.sum(0, 3);
         Assert.assertEquals(3, result);
     }
